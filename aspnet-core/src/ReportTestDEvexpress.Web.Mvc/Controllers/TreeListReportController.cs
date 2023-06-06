b0dd06df91e86762e7879e21fe3820db90195034
Hameedjamal <91000385+hameedjamal255@users.noreply.github.com>
2023-06-06 11:59:13 +0500

    TreeListReport

---
+++ aspnet-core/src/ReportTestDEvexpress.Web.Mvc/Controllers/TreeListReportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportTestDEvexpress.Controllers;
+
+namespace ReportTestDEvexpress.Web.Controllers
+{
+    public class TreeListReportController : ReportTestDEvexpressControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
